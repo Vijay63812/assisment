73bf05543537e4ae966de890f4dc0c2daa3d2df5
Vijay63812 <136681780+Vijay63812@users.noreply.github.com>
2023-06-23 11:30:09 +0530

    Add files via upload

---
+++ Swap01.java
@@ -0,0 +1,24 @@
+//swapping for three numbers with fourth variable//
+public class Swap01{
+public static void main(String args[]){
+
+int a=8;
+int b=7;
+int c=9;
+
+System.out.println("BEFORE SWAPPING FOR THREE NUMBERS");
+System.out.println("a ="+a);
+System.out.println("b ="+b);
+System.out.println("c ="+c);
+int d=a;  //d=8
+a=b;      //a=b a=7
+b=c;      //b=c b=9
+c=d;      //c=d c=8
+System.out.println("AFTER SWAPPING FOR THREE NUMBERS");
+System.out.println("a="+a);
+System.out.println("b="+b);
+System.out.println("c="+c);
+
+
+}
+}  
